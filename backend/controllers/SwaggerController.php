ae7b61a80c4111a6b5ab8023040f7e1a6764f6e3
Sammy Barasa <barsamms@gmail.com>
2019-04-14 19:50:42 +0300

    reverted swagger controller

--- backend/controllers/SwaggerController.php
+++ backend/controllers/SwaggerController.php
@@ -3,14 +3,40 @@
 namespace backend\controllers;
 
 
+use Yii;
+use yii\helpers\Url;
+use yii\web\Controller;
+
+
 /**
  * SwaggerController .
  */
-class SwaggerController extends \yii\web\Controller
+class SwaggerController extends Controller
 {
-
-    public function actionIndex()
+    /**
+     * @inheritdoc
+     */
+    public function actions(): array
     {
-        return $this->render('index');
+        return [
+            'index' => [
+                'class' => 'yii2mod\swagger\SwaggerUIRenderer',
+                'view' => '@vendor/masgeek/yii2-swagger/views/index',
+                'restUrl' => Url::to(['swagger/json-schema'], true),
+            ],
+            'json-schema' => [
+                'class' => 'yii2mod\swagger\OpenAPIRenderer',
+                // Тhe list of directories that contains the swagger annotations.
+                'scanDir' => [
+                    Yii::getAlias('@common/controllers'),
+                    Yii::getAlias('@api/modules/v1/controllers'),
+                    Yii::getAlias('@api/models'),
+                ],
+            ],
+
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+        ];
     }
 }
